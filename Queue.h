56bb3e2f63c17d4e393b77140f07f8dde3e540dc
KK <110870701+MyNameKK@users.noreply.github.com>
2024-07-03 04:09:58 +0000

    Assignment 10

--- Queue.h
+++ Queue.h
@@ -17,9 +17,15 @@ void Queue::enqueue(int x){
   NodePtr new_node= new NODE(x);
 if(new_node){ 
     /* Add head and tail for me please */
-   1. connect & Change tail
-  2. (may be) change head  when the queue is empty
-  3. increase size*/
+   //1. connect & Change tail
+   
+   if (headPtr==NULL) headPtr = new_node;
+   else tailPtr->set_next(new_node);
+   tailPtr=new_node;
+  //2. (may be) change head  when the queue is empty
+  
+  //3. increase size*/
+    size+=1;
 	
  }
 }
@@ -29,7 +35,9 @@ int Queue::dequeue(){
      NodePtr t=headPtr;
      int value= t->get_value();
      /* Add head and tail for me please */
-          
+     headPtr=headPtr->get_next();
+     if (headPtr==NULL) tailPtr=NULL;
+      size-=1;
      delete t;
      return value;
   }
@@ -40,10 +48,17 @@ int Queue::dequeue(){
 
 Queue::Queue(){
     //initialize Queue
+    headPtr = NULL;
+    tailPtr=NULL;
+    size = 0;
     
 }
 Queue::~Queue(){
     //delete all remaning Queue (i.e. DQ all) 
+    cout<<"Clearing Queue"<<endl;
+    while(headPtr!=NULL) {
+      cout<<"dequeing "<<dequeue()<<endl;
+    }
     
 }
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #include "Queue.h"
+#include <string.h>
 
 int main(int argc , char **argv) {
   Queue q;
